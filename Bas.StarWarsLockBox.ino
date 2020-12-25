7469ee6afa8cb8be3370763d800266a87ab2d7cf
Bas Paap <baspaap@live.nl>
2020-12-25 13:11:50 +0100

    Add button code.

--- Bas.StarWarsLockBox.ino
+++ Bas.StarWarsLockBox.ino
@@ -8,21 +8,25 @@
 #include "BlinkingLed.h"
 #include "Led.h"
 #include "UnlockSoundData.h"
+#include "Button.h"
 
 const int speakerPin = 11;  // Can be either 3 or 11, two PWM outputs connected to Timer 2
 const int greenLedPin = 2;
 const int redLedPin = 3;
 const int buttonPin = 4;
+const unsigned long debounceDelay = 50;
 
 Bas::Sound unlockSound(speakerPin, unlockSoundData, sizeof(unlockSoundData));
-Bas::Led greenLed(greenLedPin);
-Bas::BlinkingLed redLed(redLedPin, 200, 400);
+Bas::Led greenLed{ greenLedPin };
+Bas::BlinkingLed redLed{ redLedPin, 200, 400 };
+Bas::Button unlockButton{ buttonPin, debounceDelay };
 
 // The setup() function runs once each time the micro-controller starts
 void setup()
 {
 	Serial.begin(9600);	
 	
+	unlockButton.initialize(onUnlockButtonPressed);
 	greenLed.initialize();
 	redLed.initialize();
 	redLed.turnOn();
@@ -34,6 +38,13 @@ void loop()
 	redLed.update();
 }
 
+
+void onUnlockButtonPressed()
+{
+	redLed.turnOff();
+	greenLed.turnOn();
+	unlockSound.play();
+}
 
 // This is called at 8000 Hz to load the next sample.
 ISR(TIMER1_COMPA_vect) {
